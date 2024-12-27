a7d30a0807148fac21243d69fb6269c256ba5d19
itshamzaaah <maratib.hamza@gmail.com>
2024-12-27 23:47:33 +0500

    afj

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -74,7 +74,7 @@ export function Contact() {
                 <Phone className="text-gray-600 flex-shrink-0" />
                 <div>
                   <h4 className="font-medium">Whatsapp</h4>
-                  <p className="text-gray-600 truncate">+92313687703</p>
+                  <p className="text-gray-600 truncate">+92 313 6877063</p>
                   <a
                     href="https://wa.me/+923016639195"
                     className="text-sm text-gray-600 hover:underline"
